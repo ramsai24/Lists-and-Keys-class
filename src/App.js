c3e09b44ee997b0cc596d80ba3fe3ac0b49e5dfa
ramsai24 <jnrsai24@gmail.com>
2023-05-20 07:51:29 +0000

    class continues, single user data only

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserProfile from './components/Userprofile/index'
+
+const userDetails = {
+  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'Rahul',
+  role: 'Software Developer',
+}
+
+const App = () => <UserProfile userDetails={userDetails} />
+
+export default App
